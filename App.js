811652aecb3beba0db0ad1233a45a028271f1b18
NaveenRathore5094 <rathorenaveen121@gmail.com>
2024-12-23 16:38:24 +0530

    fixed

--- App.js
+++ App.js
@@ -1,34 +1,33 @@
-// Async - Await Example 
+// Async - Await Example
 
-function data(id){
-  return new Promise((reslove,reject)=>{
-    setTimeout(()=>{
+function data(id) {
+  return new Promise((reslove, reject) => {
+    setTimeout(() => {
       console.log(`Data : ${id}`);
-      reslove("success")
-      
-    },3000)
-  })
+      reslove("success");
+    }, 3000);
+  });
 }
-async function Alldata(){
-  await data("virat Kohli")
-  await data("Rohit Sharma")
+async function Alldata() {
+  await data("virat Kohli");
+  await data("Rohit Sharma");
 }
-Alldata()
+Alldata();
 
-     // Promise Chaining
+// Promise Chaining
 
-function mydata(id){
-  return new Promise((reslove,reject)=>{
-    setTimeout(()=>{
+function mydata(id) {
+  return new Promise((reslove, reject) => {
+    setTimeout(() => {
       console.log(`Data : ${id}`);
-      reslove("success")
-      
-    },3000)
-  })
+      reslove("success");
+    }, 3000);
+  });
 }
-data("kabir singh").then(()=>{
-  return data("Arjun reddy")
-}).then((res)=>{
-  console.log(res);
-  
-})
+data("kabir singh")
+  .then(() => {
+    return data("Arjun reddy");
+  })
+  .then((res) => {
+    console.log(res);
+  });
